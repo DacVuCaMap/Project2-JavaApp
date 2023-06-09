8f2825b66511a4a1f82118cfb9e5537a98e6f7fb
DacVuCaMap <namvu04112k@gmail.com>
2023-06-09 16:16:43 +0700

    little change

--- app/src/main/java/com/example/app/HelloApplication.java
+++ app/src/main/java/com/example/app/HelloApplication.java
@@ -22,7 +22,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         LoginScene.getStage = stage;
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sceneView/SelectMenuScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sceneView/LoginScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setResizable(false);
         stage.setTitle("Apartment Management");
